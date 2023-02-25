cfbd5fbd15fc385283af38830f4a7c84e4895e93
Giovani <giovanibulian@hotmail.com>
2023-02-24 20:47:08 -0400

    + password reset

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -140,3 +140,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASS'))
